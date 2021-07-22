629f33c87fdfab90c397869cbfe42995128e9c4a
achiyazigi <matanelcohen0@walla.com>
2021-07-22 15:40:27 +0000

    added models

---
+++ AlephBERT-main/models/alephbert-base/english-model.py
@@ -0,0 +1,41 @@
+import torch
+import sys
+from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
+
+# OPTIONAL: if you want to have more information on what's happening, activate the logger as follows
+import logging
+logging.basicConfig(level=logging.INFO)
+
+# Load pre-trained model tokenizer (vocabulary)
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+sentence = sys.argv[1]
+mask = sys.argv[2]
+text = '[CLS]' + sentence + ' . [SEP]'
+tokenized_text = tokenizer.tokenize(text)
+indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+
+# Create the segments tensors.
+segments_ids = [0] * len(tokenized_text)
+
+# Convert inputs to PyTorch tensors
+tokens_tensor = torch.tensor([indexed_tokens])
+segments_tensors = torch.tensor([segments_ids])
+
+# Load pre-trained model (weights)
+model = BertForMaskedLM.from_pretrained('bert-base-uncased')
+model.eval()
+
+# Predict all tokens
+with torch.no_grad():
+    predictions = model(tokens_tensor, segments_tensors)
+
+masked_index = tokenized_text.index(mask)
+
+predicted_sorted = torch.argsort(predictions[0, masked_index], descending=True)
+topres = tokenizer.convert_ids_to_tokens([x.item() for x in predicted_sorted[:5]])
+
+predicted_index = torch.argmax(predictions[0, masked_index]).item()
+predicted_token = tokenizer.convert_ids_to_tokens([predicted_index])[0]
+
+# print(predicted_token)
+print(topres)
---
+++ AlephBERT-main/models/alephbert-base/test_load_model.py
@@ -0,0 +1,42 @@
+from transformers import BertModel, BertTokenizerFast
+
+alephbert_tokenizer = BertTokenizerFast.from_pretrained('onlplab/alephbert-base')
+alephbert = BertModel.from_pretrained('onlplab/alephbert-base')
+# if not finetuning - disable dropout
+
+
+import torch
+
+hebrew_text = '[CLS] לכולם שלום [MASK] מאוד . [SEP]'
+
+tokenized_text = alephbert_tokenizer.tokenize(hebrew_text)
+indexed_tokens = alephbert_tokenizer.convert_tokens_to_ids(tokenized_text)
+
+
+# Create the segments tensors.
+segments_ids = [0] * len(tokenized_text)
+
+# Convert inputs to PyTorch tensors
+tokens_tensor = torch.tensor([indexed_tokens])
+
+segments_tensors = torch.tensor([segments_ids])
+
+# Load pre-trained model (weights)
+alephbert.eval()
+
+# Predict all tokens
+with torch.no_grad():
+    predictions = alephbert(tokens_tensor, segments_tensors)['last_hidden_state']
+
+masked_index = tokenized_text.index('[MASK]')
+
+predicted_index = torch.argmax(predictions[0,masked_index]).item()
+
+predicted_sorted = torch.argsort(predictions[0, masked_index], descending=True)
+
+predicted_token = alephbert_tokenizer.convert_ids_to_tokens([predicted_index])[0]
+
+print(predicted_token)
+# print(alephbert_tokenizer.convert_ids_to_tokens([token.item() for token in predicted_sorted[:20]]))
+print([token.item() for token in predicted_sorted[:20]])
+print(hebrew_text)
