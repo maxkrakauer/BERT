4592103b66f76a0088ecb7c835a977d5e8562f16
achiyazigi <matanelcohen0@walla.com>
2021-07-26 01:07:15 +0300

    for or trains

--- AlephBERT-main/models/alephbert-base/english-model.py
+++ AlephBERT-main/models/alephbert-base/english-model.py
@@ -1,17 +1,17 @@
-import torch
-import sys
-from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
+import torch, sys
+from transformers import BertTokenizerFast, BertForMaskedLM
 
 # OPTIONAL: if you want to have more information on what's happening, activate the logger as follows
 import logging
 logging.basicConfig(level=logging.INFO)
 
 # Load pre-trained model tokenizer (vocabulary)
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
+
 sentence = sys.argv[1]
 mask = sys.argv[2]
 sentence = sentence.replace(mask, '[MASK]')
-print(sentence)
+# print(sentence)
 text = '[CLS] ' + sentence + ' . [SEP]'
 tokenized_text = tokenizer.tokenize(text)
 indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
@@ -33,11 +33,20 @@ with torch.no_grad():
 
 masked_index = tokenized_text.index('[MASK]')
 
-predicted_sorted = torch.argsort(predictions[0, masked_index], descending=True)
+predicted_sorted = torch.argsort(predictions[0][0, masked_index], descending=True)
 topres = tokenizer.convert_ids_to_tokens([x.item() for x in predicted_sorted[:5]])
 
-predicted_index = torch.argmax(predictions[0, masked_index]).item()
-predicted_token = tokenizer.convert_ids_to_tokens([predicted_index])[0]
+def probs_from_predictions(predictions):
+    min_prediction = -predictions[0][0, masked_index][predicted_sorted[-1]]
+    sum_of_predictions = torch.sum(predictions[0][0, masked_index])
+    return (predictions[0][0, masked_index] + min_prediction) / (sum_of_predictions + min_prediction * len(predicted_sorted))
 
-# print(predicted_token)
-print(topres)
+probs = probs_from_predictions(predictions)
+
+sum_of_probs = torch.sum(probs)
+assert(abs(sum_of_probs - 1) < 0.00001)
+
+nice_probs = ['{:.7f}'.format(x.item()) for x in probs]
+
+with_prob = [(res, nice_probs[tokenizer.convert_tokens_to_ids([res])[0]]) for res in topres]
+print(with_prob)
